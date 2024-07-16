fdf70fa2bf1a39e96fa4cd5914a7c1c6e789c93c
AmarPatil <mail2amarp@gmail.com>
2024-07-15 21:44:50 -0400

    Porject 2 update 07/15/2024

---
+++ Crowdfunding_ETL/01-Ins_ETL_Mini_Project/Starter_Files/QuickDBD_ERD_project 2_group15.sql
@@ -0,0 +1,69 @@
+﻿-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
+-- Link to schema: https://app.quickdatabasediagrams.com/#/d/KCnfLJ
+-- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
+
+-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
+-- Link to schema: https://app.quickdatabasediagrams.com/
+-- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
+
+CREATE TABLE "category" (
+    "category_id" VARCHAR(4)   NOT NULL,
+    "category" VARCHAR(20)   NOT NULL,
+    "last_updated" timestamp   NOT NULL,
+    CONSTRAINT "pk_category" PRIMARY KEY (
+        "category_id"
+     )
+);
+
+CREATE TABLE "subcategory" (
+    "subcategory_id" VARCHAR(8)   NOT NULL,
+    "subcategory" VARCHAR(20)   NOT NULL,
+    "last_updated" timestamp   NOT NULL,
+    CONSTRAINT "pk_subcategory" PRIMARY KEY (
+        "subcategory_id"
+     )
+);
+
+CREATE TABLE "contact" (
+    "contact_id" FLOAT(4)   NOT NULL,
+    "first_name" VARCHAR(20)   NOT NULL,
+    "last_name" VARCHAR(20)   NOT NULL,
+    "email" VARCHAR(80)   NOT NULL,
+    "last_updated" timestamp   NOT NULL,
+    CONSTRAINT "pk_contact" PRIMARY KEY (
+        "contact_id"
+     )
+);
+
+CREATE TABLE "campaign" (
+    "cf_id" FLOAT(4)   NOT NULL,
+    "contact_id" FLOAT(4)   NOT NULL,
+    "company_name" VARCHAR(120)   NOT NULL,
+    "description" VARCHAR(120)   NOT NULL,
+    "goal" FLOAT(10)   NOT NULL,
+    "pledged" FLOAT(10)   NOT NULL,
+    "outcome" VARCHAR(12)   NOT NULL,
+    "backers_count" FLOAT(10)   NOT NULL,
+    "country" VARCHAR(4)   NOT NULL,
+    "currency" VARCHAR(4)   NOT NULL,
+    "launch_date" date   NOT NULL,
+    "end_date" date   NOT NULL,
+    "staff_pick" bool   NOT NULL,
+    "spotlight" bool   NOT NULL,
+    "category_id" VARCHAR(4)   NOT NULL,
+    "subcategory_id" VARCHAR(20)   NOT NULL,
+    "last_updated" timestamp   NOT NULL,
+    CONSTRAINT "pk_campaign" PRIMARY KEY (
+        "cf_id"
+     )
+);
+
+ALTER TABLE "campaign" ADD CONSTRAINT "fk_campaign_contact_id" FOREIGN KEY("contact_id")
+REFERENCES "contact" ("contact_id");
+
+ALTER TABLE "campaign" ADD CONSTRAINT "fk_campaign_category_id" FOREIGN KEY("category_id")
+REFERENCES "category" ("category_id");
+
+ALTER TABLE "campaign" ADD CONSTRAINT "fk_campaign_subcategory_id" FOREIGN KEY("subcategory_id")
+REFERENCES "subcategory" ("subcategory_id");
+
